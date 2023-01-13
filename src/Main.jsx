b681a1b6e2529ad4bc63022734f322c78b3e9f6b
Vagapova Valeriia <lera.vagapova@gmail.com>
2023-01-13 14:48:40 +0200

    add images

--- src/Main.jsx
+++ src/Main.jsx
@@ -4,16 +4,35 @@ import MyCard from "./MyCard";
 
 const Main = () => {
   const [cards, setCards] = useState([
-    { cardTitle: "card 1", description: "description1" },
-    { cardTitle: "card 2", description: "description2" },
-    { cardTitle: "card 3", description: "description3" },
+    {
+      cardTitle: "card 1",
+      description: "description1",
+      image:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg",
+    },
+    {
+      cardTitle: "card 2",
+      description: "description2",
+      image:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg",
+    },
+    {
+      cardTitle: "card 3",
+      description: "description3",
+      image:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg",
+    },
   ]);
 
   return (
     <main>
       <h1>Main</h1>
       {cards.map((card) => (
-        <MyCard title={card.cardTitle} description={card.description} />
+        <MyCard
+          title={card.cardTitle}
+          description={card.description}
+          image={card.image}
+        />
       ))}
     </main>
   );
--- src/MyCard.jsx
+++ src/MyCard.jsx
@@ -3,10 +3,10 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
-const MyCard = ({ title, text }) => {
+const MyCard = ({ title, text, image }) => {
   return (
     <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Img variant="top" src={image} />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Text>{text}</Card.Text>
