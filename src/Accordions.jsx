2ce2e52927f7c63c0ae059108bc4fe243ba2f8fe
Vagapova Valeriia <lera.vagapova@gmail.com>
2023-01-20 11:14:51 +0200

    fix accordion

--- src/Accordions.jsx
+++ src/Accordions.jsx
@@ -6,12 +6,12 @@ import AccordionHeader from "react-bootstrap/esm/AccordionHeader";
 const Accordions = () => {
   return (
     <div class="accordion" id="accordionExample">
-      <Accordion defaultActiveKey="0">
+      <Accordion defaultActiveKey="0" flush alwaysOpen>
         <Accordion.Item eventKey="0">
           <Accordion.Header>This will be open by default</Accordion.Header>
           <Accordion.Body>Heya</Accordion.Body>
         </Accordion.Item>
-        <Accordion.Item eventKey="0">
+        <Accordion.Item eventKey="1">
           <Accordion.Header>
             What is the meaning of the universe, life and everything?
           </Accordion.Header>
